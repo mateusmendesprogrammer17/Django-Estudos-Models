ba43014fd0ca6ca3956c18bf7f1da138ad21eb12
Mateus Mendes <116766162+mateusmendesprogrammer17@users.noreply.github.com>
2024-10-18 13:52:06 -0300

    Updated views.py

--- django-mariadb/users/views.py
+++ django-mariadb/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import AlunosForm
 from .models import Aluno,Curso
+from django.db.models import Count
 
 def home(request):
     if request.method == 'POST':
@@ -19,5 +20,20 @@ def lista_alunos(request):
     return render(request, 'users/lista_alunos.html', {'alunos': alunos})
 
 def cursos(request):
-    cursos = Curso.objects.all()
-    return HttpResponse("Cursos")
+    cursos = Curso.objects.annotate(qtd_alunos=Count('alunos')).all()
+    return render(request,'users/lista_cursos.html',{'cursos':cursos})
+
+from django.shortcuts import render
+from .models import Aluno
+
+def whatCourse(request):
+    try:
+        students = Aluno.objects.all().select_related('curso')
+    except Exception as e:
+        # Log a mensagem de erro ou tratar a exceção conforme necessário
+        students = []  # Ou lidar de outra forma
+
+    return render(request, 'users/whatcourse.html', {
+        'students': students,
+        'total_students': students.count(),
+    })
