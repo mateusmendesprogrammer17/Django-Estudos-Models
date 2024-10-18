27055abbea0bb396aa61bfcc01ac01c45e983590
Mateus Mendes <116766162+mateusmendesprogrammer17@users.noreply.github.com>
2024-10-18 13:54:48 -0300

    Updated models.py

--- django-mariadb/users/models.py
+++ django-mariadb/users/models.py
@@ -13,12 +13,13 @@ class Curso(models.Model):
     carga_horaria = models.IntegerField() #quantidade de horas
     data_termino=models.DateField()
     duracao = models.CharField(max_length=50) # duracao = models.CharField(max_length=50)  # por exemplo, "6 semanas"
+   
     def __str__(self):
         return self.nome
 class Aluno(models.Model):
     nome = models.CharField(max_length=100 , null=False) # not null e varchar(100)
     email = models.EmailField(max_length=150,unique=True) # unique
-    curso = models.ForeignKey(Curso,on_delete=models.CASCADE, null=True)
+    curso = models.ForeignKey(Curso,on_delete=models.CASCADE, null=True,related_name='alunos')
     
     def __str__(self):
         return self.nome 
